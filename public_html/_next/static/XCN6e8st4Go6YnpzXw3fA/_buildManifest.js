cd816fcfca94280e9240ef8d86be54e628c3bc88
bokar83 <bokar83@gmail.com>
2025-09-24 19:55:11 +0400

    Refactor: static deploy ready (public_html, .htaccess, 404, robots, sitemap, http-server)

---
+++ public_html/_next/static/XCN6e8st4Go6YnpzXw3fA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/124-464000b304b8ae6d.js","static/css/f905e3f5554554b8.css","static/chunks/pages/index-7d0261209127b521.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
